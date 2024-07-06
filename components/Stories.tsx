e1403f65adba4130feba788a7a0ef17a3db79726
robiulsagor <robiul100.me@gmail.com>
2024-07-06 21:13:32 +0600

    stories section done

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,5 +1,194 @@
+import Image from "next/image";
+
 function Stories() {
-  return <div>Stories</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm scroll-hide">
+      <div className="flex gap-8 w-max">
+        {/* story */}
+        <div className="flex flex-col  gap-4 items-center  cursor-pointer">
+          <Image
+            src={
+              "https://images.pexels.com/photos/15804651/pexels-photo-15804651/free-photo-of-people-together-on-motorcycle-on-road-in-mountains.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>robi.</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/26754397/pexels-photo-26754397/free-photo-of-sunrise-praia-do-felix.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/19082533/pexels-photo-19082533/free-photo-of-view-of-a-sunrise-from-a-flying-airplane.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/25972597/pexels-photo-25972597/free-photo-of-photographer-shadow-on-arid-barren-rocks.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/26771975/pexels-photo-26771975/free-photo-of-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/26077583/pexels-photo-26077583/free-photo-of-a-lone-tree-stands-on-top-of-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center w-max cursor-pointer">
+          <Image
+            src={
+              "https://images.pexels.com/photos/15804651/pexels-photo-15804651/free-photo-of-people-together-on-motorcycle-on-road-in-mountains.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/26754397/pexels-photo-26754397/free-photo-of-sunrise-praia-do-felix.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/19082533/pexels-photo-19082533/free-photo-of-view-of-a-sunrise-from-a-flying-airplane.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/25972597/pexels-photo-25972597/free-photo-of-photographer-shadow-on-arid-barren-rocks.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/26771975/pexels-photo-26771975/free-photo-of-sunset.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={
+              "https://images.pexels.com/photos/26077583/pexels-photo-26077583/free-photo-of-a-lone-tree-stands-on-top-of-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            }
+            height={80}
+            width={80}
+            className="w-16 h-16 rounded-full ring-2 shadow-lg  cursor-pointer"
+            alt="story"
+          />
+          <span>Robiul</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stories;
